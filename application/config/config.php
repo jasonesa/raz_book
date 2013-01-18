a2ab656eaa10fa2b337d0222c73e1a1390fc9c1b
fred <fred.aponte@gmail.com>
2013-01-18 09:40:03 -0600

    aponte

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/raz_book';
+$config['base_url']	= 'http://localhost:8888/raz_book';
 
 /*
 |--------------------------------------------------------------------------
