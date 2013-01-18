7ad6fb9b8bee2994fbe79daf96c2ed23898c8ca9
fred <fred.aponte@gmail.com>
2013-01-18 16:02:17 -0600

    ponte

--- application/views/resource_view.php
+++ application/views/resource_view.php
@@ -38,14 +38,16 @@
                 <!-- This contains the hidden content for inline calls -->
                 <div style='display:none'>
                     <div id='inline_content' style='padding:10px; background:#fff;'>
-                    <p>You are about to book this resurses from <?php echo urldecode($starts);?> to <?php echo urldecode($ends);?></p>
+                    <p>You are about to book this resources from <?php echo urldecode($starts);?> to <?php echo urldecode($ends);?></p>
                     <form action="<?php echo base_url()?>reservation/book_resource" method="post">
                         <input type="hidden" name="resource_id" value="<?php echo $resource->idresource;?>"/>
                         <input type="hidden" name="start_date" value="<?php echo urldecode($starts);?>"/>
                         <input type="hidden" name="end_date" value="<?php echo urldecode($ends);?>"/>
+                        <input type="text" name="project_name" placeholder="Name or Description" />
+
                         <input type="submit" class="cta bookNow" value="Book"/>
                     </form>
-                    <p>Or <a id="click" href="<?php echo base_url()?>reservation/all">Add to existent reservation</a></p>
+                    <p>Or <a id="click" class="cta" href="<?php echo base_url()?>reservation/all">Add to existent reservation</a></p>
                     </div>
                 </div>
             </div>
