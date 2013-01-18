bc245624ef2d9628ff24db33befea08c83a380d9
fred <fred.aponte@gmail.com>
2013-01-18 10:55:21 -0600

    a

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
-$(function() {
-	$( "#datepicker, #datepickerTwo, .datePicker").datepicker();
+$(function(){
+	//$( "#datepicker, #datepickerTwo, .datePicker").datepicker();
 
-	$('.confirm').colorbox({
-        href:$(this).attr('href'), 
-        rel: $(this).attr('rel'),
-        scrolling:false
-    }, function(){
-        
-    });
+	$('.confirm').colorbox({inline:true}, function(){});
 });
