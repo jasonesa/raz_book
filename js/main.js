130abf09eb9863bb496b8168210a7197cd0c0bf0
fred <fred.aponte@gmail.com>
2013-01-18 10:06:22 -0600

    modal

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
- $(function() {
-                $( "#datepicker, #datepickerTwo, .datePicker").datepicker();
-           });
+$(function() {
+	$( "#datepicker, #datepickerTwo, .datePicker").datepicker();
+
+	$('.confirm').colorbox({
+        href:$(this).attr('href'), 
+        rel: $(this).attr('rel'),
+        scrolling:false
+    }, function(){
+        
+    });
+});
