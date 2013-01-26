21071270ba7c535566cc6bbc3b4924410446715a
Jason zumbado <jason.zumbado@boszdigital.com>
2013-01-26 00:14:24 -0600

    update view to display resources in different columns(teams)

--- application/views/resources_view.php
+++ application/views/resources_view.php
@@ -3,74 +3,48 @@
 <div role="main">
 	<h1>Bosz Digital Team</h1>
 	<!-- ------------------------------------------------------------------------------ -->
-	<div class="column-one-third maxHeight">
-		<?php foreach($available_resources as $resource):?>
-		<section class="preview">
-
-			<img width="55" height="55" src="<?php echo base_url();?>images/boszbook-demo-img.jpg"/>
-			<p>
-				<strong><?php echo $resource->name?></strong>
-				<input type="checkbox" name="" value="">
-			</p>
-			<p>
-				<?php echo $resource->position?>
-			</p>
-			<ul>
-				<li>
+	
+	<?php
+	//Separate resources by teams(html columns), could've been done with templates, choose this way because of performance
+	$continue = true;
+	$continue_inner = true;
+	$current_team = $available_resources[1] -> table_id_team;
+	while ($continue):
+		$continue_inner = true;?>
+		<div class="column-one-third maxHeight">
+		<?php
+		while ($continue_inner):
+			$resource = current($available_resources);
+			if ($resource -> table_id_team != $current_team):
+				$current_team = $resource -> table_id_team;
+				$continue_inner = false;
+				prev($available_resources);
+			else:?>
+				<section class="preview">
+					<img width="55" height="55" src="<?php echo base_url(); ?>images/boszbook-demo-img.jpg"/>
+					<p>
+					<strong><?php echo $resource->name?></strong>
+					<input type="checkbox" name="" value="">
+					</p>
+					<p><?php echo $resource -> position; ?></p>
 					<a href="<?php echo base_url().'resource/'. $resource->idresource.'/'.$starts.'/'.$ends?>">View more</a>
-				</li>				
-			</ul>
-		</section>
-		<?php endforeach; ?>
-	</div>
+				</section>
+			<?php endif;
+					next($available_resources);
+					if (key($available_resources) === null) {
+					$continue = false;
+					$continue_inner = false;
+					}
+		endwhile;?>
+		</div>
+	<?php endwhile; ?>
+	
 	<!-- ------------------------------------------------------------------------------ -->
 
-	<div class="column-one-third maxHeight">
-		<?php foreach($available_resources as $resource):?>
-		<section class="preview">
-
-			<img width="55" height="55" src="<?php echo base_url();?>images/boszbook-demo-img.jpg"/>
-			<p>
-				<strong><?php echo $resource->name?></strong>
-				<input type="checkbox" name="" value="">
-			</p>
-			<p>
-				<?php echo $resource->position?>
-			</p>
-			<ul>
-				<li>
-					<a href="<?php echo base_url().'resource/'. $resource->idresource?>">View more</a>
-				</li>
-			</ul>
-		</section>
-		<?php endforeach; ?>
-	</div>
-	<!-- ------------------------------------------------------------------------------ -->
-	<div class="column-one-third maxHeight">
-		<?php foreach($available_resources as $resource):?>
-		<section class="preview">
-
-			<img width="55" height="55" src="<?php echo base_url();?>images/boszbook-demo-img.jpg"/>
-			<p>
-				<strong><?php echo $resource->name?></strong>
-				<input type="checkbox" name="" value="">
-			</p>
-			<p>
-				<?php echo $resource->position?>
-			</p>
-			<ul>
-				<li>
-					<a href="<?php echo base_url().'resource/'. $resource->idresource?>">View more</a>
-				</li>
-			</ul>
-		</section>
-		<?php endforeach; ?>
-	</div>
-	<!-- ------------------------------------------------------------------------------ -->
 	<form id="bookMore" action="" method="post">
-	    <input type="hidden" name="resource_id" value="<?php echo $resource->idresource;?>"/>
-	    <input type="hidden" name="start_date" value="<?php echo urldecode($starts);?>"/>
-	    <input type="hidden" name="end_date" value="<?php echo urldecode($ends);?>"/>
+	    <input type="hidden" name="resource_id" value="<?php echo $resource -> idresource; ?>"/>
+	    <input type="hidden" name="start_date" value="<?php echo urldecode($starts); ?>"/>
+	    <input type="hidden" name="end_date" value="<?php echo urldecode($ends); ?>"/>
 	    <input type="text" name="project_name" placeholder="Add a name or description for the project" />
 
 	    <input type="submit" class="cta bookNow" value="Book all"/>
