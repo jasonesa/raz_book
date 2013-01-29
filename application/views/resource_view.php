23dba84341b04c12e7b8e04b31cc6826d933f3a1
jason <jason.zumbado@boszdigital.com>
2013-01-29 09:20:46 -0800

    para fredito arregle el modal

--- application/views/resource_view.php
+++ application/views/resource_view.php
@@ -37,6 +37,7 @@
                 <!-- Ends Sidebar -->
                 <!-- This contains the hidden content for inline calls -->
                 <div style='display:none'>
+                	 <?php if($starts!=null):?>
                     <div id='inline_content' style='padding:10px; background:#fff;'>
                     <p>You are about to book this resources from <?php echo urldecode($starts);?> to <?php echo urldecode($ends);?></p>
                     <form action="<?php echo base_url()?>reservation/book_resource" method="post">
@@ -44,10 +45,16 @@
                         <input type="hidden" name="start_date" value="<?php echo urldecode($starts);?>"/>
                         <input type="hidden" name="end_date" value="<?php echo urldecode($ends);?>"/>
                         <input type="text" name="reservation_name" placeholder="Add a name or description for the project" />
-
-                        <input type="submit" class="cta bookNow" value="Book Now"/>  or  <a id="click" class="cta" href="<?php echo base_url()?>reservation/all">Add to existent reservation</a>
+                        <input type="submit" class="cta bookNow" value="Book Now"/>  or  
+                        <a id="click" class="cta" href="<?php echo base_url()?>reservation/all">Add to existent reservation</a>
                     </form>
+                    
                     </div>
+                   <?php endif;?>
+                    <div id='inline_content' style='padding:10px; background:#fff;'>
+                    	fredito es un baby
+                    </div>
+                    
                 </div>
             </div>
             <!-- Ends Main Container -->
