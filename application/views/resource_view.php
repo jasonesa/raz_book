0b721b17935b40802a334c8ac4ad20b6ec2ff659
fred <fred.aponte@gmail.com>
2013-01-29 10:57:03 -0600

    msj

--- application/views/resource_view.php
+++ application/views/resource_view.php
@@ -37,22 +37,29 @@
                 <!-- Ends Sidebar -->
                 <!-- This contains the hidden content for inline calls -->
                 <div style='display:none'>
-                	 <?php if($starts!=null && $end!=null):?>
+                	<?php if($starts!=null && $end!=null):?>
                     <div id='inline_content' style='padding:10px; background:#fff;'>
-                    <p>You are about to book this resources from <?php echo urldecode($starts);?> to <?php echo urldecode($ends);?></p>
-                    <form action="<?php echo base_url()?>reservation/book_resource" method="post">
-                        <input type="hidden" name="resource_id" value="<?php echo $resource->idresource;?>"/>
-                        <input type="hidden" name="start_date" value="<?php echo urldecode($starts);?>"/>
-                        <input type="hidden" name="end_date" value="<?php echo urldecode($ends);?>"/>
-                        <input type="text" name="reservation_name" placeholder="Add a name or description for the project" />
-                        <input type="submit" class="cta bookNow" value="Book Now"/>  or  
-                        <a id="click" class="cta" href="<?php echo base_url()?>reservation/all">Add to existent reservation</a>
-                    </form>
-                    
+                        <p>You are about to book this resources from <?php echo urldecode($starts);?> to <?php echo urldecode($ends);?></p>
+                        <form action="<?php echo base_url()?>reservation/book_resource" method="post">
+                            <input type="hidden" name="resource_id" value="<?php echo $resource->idresource;?>"/>
+                            <input type="hidden" name="start_date" value="<?php echo urldecode($starts);?>"/>
+                            <input type="hidden" name="end_date" value="<?php echo urldecode($ends);?>"/>
+                            <input type="text" name="reservation_name" placeholder="Add a name or description for the project" />
+                            <input type="submit" class="cta bookNow" value="Book Now"/>  or  
+                            <a id="click" class="cta" href="<?php echo base_url()?>reservation/all">Add to existent reservation</a>
+                        </form>
                     </div>
-                   <?php endif;?>
+                    <?php endif;?>
                     <div id='inline_content' style='padding:10px; background:#fff;'>
-                    	fredito es un baby
+                    	<form action="<?php echo base_url()?>reservation/book_resource" method="post">
+                            <input type="hidden" name="resource_id" value="<?php echo $resource->idresource;?>"/>
+                            <p>You are about to book this resources from </p>
+                            <input class="shortBox datePicker" name="start_date" type="text" placeholder="Select a Date" /><p> to </p>
+                            <input class="shortBox datePicker" name="end_date" type="text" placeholder="Select a Date" />
+                            <input type="text" class="projectName" name="reservation_name" placeholder="Add a name or description for the project" />
+                            <!-- <input type="submit" class="cta bookNow" value="Book Now"/>  or  
+                            <a id="click" class="cta" href="<?php echo base_url()?>reservation/all">Add to existent reservation</a> -->
+                        </form>
                     </div>
                     
                 </div>
